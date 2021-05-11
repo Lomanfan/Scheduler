d8e6a1e2ccd127663791fd1fb6a434f2e069465b
Lomanfan <lfan.canada@gmail.com>
2021-05-10 23:42:43 -0700

    add function to format/demo spot status accordingly

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -4,8 +4,18 @@ import "components/DayListItem.scss";
 
 export default function DayListItem(props) {
 
-  const dayClass = classNames({
-    'day-list__item': true,
+  const formatSpots = (spots) => {
+    switch(spots) {
+      case 0:
+        return 'no spots remaining';
+      case 1:
+        return '1 spot remaining';
+      default:
+        return `${spots} spots remaining`;
+    }
+  };
+
+  const dayClass = classNames('day-list__item', {
     'day-list__item--selected': props.selected,
     'day-list__item--full': props.spots === 0
   });
@@ -13,7 +23,7 @@ export default function DayListItem(props) {
   return (
     <li className={dayClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots}</h3>
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
   );
 }
