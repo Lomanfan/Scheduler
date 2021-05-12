cc9184c983f8a67b21ed6205d5452d2eb59a0a23
Lomanfan <lfan.canada@gmail.com>
2021-05-11 21:50:25 -0700

    add Appointment components

---
+++ src/components/Appointment/Empty.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Empty(props) {
+
+  return (
+    <main className="appointment_add">
+      <img
+        className="appointment_add-button"
+        src="images/add.png"
+        alt="Add"
+        onClick={props.onAdd}
+      />
+    </main>
+  );
+
+}
