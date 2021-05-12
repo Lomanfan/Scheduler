eabe172e489b436b6f18ba44e3f99f32d38822c2
Lomanfan <lfan.canada@gmail.com>
2021-05-12 07:50:13 -0700

    update Appointment componenets

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { Fragment } from "react";
+import Header from "components/Appointment/Header";
+import Empty from "components/Appointment/Empty";
+import Show from "components/Appointment/Show";
 import "./styles.scss";
 
 export default function Appointment(props) {
 
   return(
-  <article className="appointment"></article>
+  <article className="appointment">
+    <Header time={props.time}/>
+    {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer}/> : <Empty />}
+  </article>
 
   );
 };
