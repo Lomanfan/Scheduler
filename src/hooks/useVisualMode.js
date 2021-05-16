1d3ecb9f522ee99056ef3fb78238a630ece3d483
Lomanfan <lfan.canada@gmail.com>
2021-05-16 11:53:44 -0700

    remove duplicate .catch for edit and delete

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -6,8 +6,8 @@ export default function useVisualMode(initial) {
 
   const transition = (newMode, replace = false) => { //default parameter
     setHistory(prev => replace ? [...prev.splice(0, prev.length - 1), newMode] : [...prev, newMode])
-    console.log("Transition History", history);
-  };
+    // console.log("Transition History", history);
+  };   //TODO//
 
   const back = () => {
     if (history.length > 1) {    //history >=1;
