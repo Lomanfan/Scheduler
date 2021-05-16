a9cfdc5e4ad8a4d889c35a1f195deb104d4a2c34
Lomanfan <lfan.canada@gmail.com>
2021-05-16 00:59:24 -0700

    remove spaces, comments

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -3,7 +3,7 @@ import "./InterviewerList.scss";
 import InterviewerListItem from "./InterviewerListItem";
 
 export default function InterviewerList(props) {
-  console.log("InterviewerList-props", props)
+  // console.log("InterviewerList-props", props)
 
   const interviewers = props.interviewers.map((interviewer) => {
     return (
