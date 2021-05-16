cf3e85c58ea453db97e899eaccc7f95d75daefcf
Lomanfan <lfan.canada@gmail.com>
2021-05-15 21:06:39 -0700

    add delete transition

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,24 +1,26 @@
 import React from "react";
+import "./styles.scss";
+import useVisualMode from "hooks/useVisualMode";
 import Header from "./Header";
 import Empty from "./Empty";
 import Show from "./Show";
-import "./styles.scss";
-import useVisualMode from "hooks/useVisualMode";
 import Form from "./Form";
 import Status from "./Status";
+import Confirm from "./Confirm";
 
 
 const EMPTY = "EMPTY";
 const SHOW = "SHOW";
 const CREATE = "CREATE";
 const SAVING = "SAVING";
+const DELETING = "DELETING";
+const CONFIRM = "CONFIRM";
 
 export default function Appointment(props) {
   // console.log("Appointment-props", props)
   // console.log("Appointment-props.interview", props.interview)
 
   const {mode, transition, back} = useVisualMode(props.interview ? SHOW : EMPTY);
-
 
   const save = (name, interviewer) => {
     const interview = {
@@ -29,8 +31,14 @@ export default function Appointment(props) {
     props.bookInterview(props.id, interview).then(() => transition(SHOW));
   }
 
+  const deleteAppointment = (id) => {
+    transition(DELETING);
+    props.cancelInterview(id)
+    .then(() => transition(EMPTY))
+  }
+  
   return(
-  <article className="appointment">
+    <article className="appointment">
     <Header time={props.time}/>
     {/* {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer}/> : <Empty />} */}
 
@@ -38,24 +46,36 @@ export default function Appointment(props) {
     {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
     {mode === SHOW && (
       <Show
-        student={props.interview.student}
-        interviewer={props.interview.interviewer}
+      student={props.interview.student}
+      interviewer={props.interview.interviewer}
+      onDelete={() => {transition(CONFIRM)}}
       />
-    )}
+      )}
     {mode === CREATE && (
       <Form
       interviewers={props.interviewers}
       onSave={save}
       onCancel={back}
       />
-    )}
+      )}
     {mode === SAVING && (
       <Status 
       message="Saving..."
       />
-    )}
-  </article>
+      )}
+    {mode === DELETING && (
+      <Status 
+      message="Deleting..."
+      />
+      )}
+      {mode === CONFIRM && (
+      <Confirm
+      message="Confirm to proceed."
+      onCancel={back}
+      onConfirm={deleteAppointment}
+      />
+      )}
 
+    </article>
   );
-
 };
