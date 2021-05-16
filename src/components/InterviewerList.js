bcee2504e0071d5e894501e45ddde7afd1c614c6
Lomanfan <lfan.canada@gmail.com>
2021-05-15 23:03:52 -0700

    update JSX on selected and set props.onChange

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,7 +1,6 @@
 import React from "react";
-import "components/InterviewerListItem.scss";
+import "./InterviewerList.scss";
 import InterviewerListItem from "./InterviewerListItem";
-
 
 export default function InterviewerList(props) {
   console.log("InterviewerList-props", props)
@@ -12,9 +11,9 @@ export default function InterviewerList(props) {
         key={interviewer.id}
         name={interviewer.name}
         avatar={interviewer.avatar}
-        selected={interviewer.id === props.interviewer}
+        selected={interviewer.id === props.value}
         // setInterviewer={setInterviewer}
-        setInterviewer={(event) => props.setInterviewer(interviewer.id)}
+        setInterviewer={() => props.onChange(interviewer.id)}
       />
     );
   })
