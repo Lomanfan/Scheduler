78d2da9cc1cae84a341d464b489d17df88895e28
Lomanfan <lfan.canada@gmail.com>
2021-05-17 01:05:18 -0700

    remove duplication on file import

--- src/index.js
+++ src/index.js
@@ -4,7 +4,5 @@ import ReactDOM from "react-dom";
 import "index.scss";
 
 import Application from "components/Application";
-import DayListItem from "components/DayListItem";
-import DayList from "components/DayList";
 
 ReactDOM.render(<Application />, document.getElementById("root"));
