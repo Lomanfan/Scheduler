751ce5d92ffd97be9b62e43c7c4eb1ec6ce6a9f7
Lomanfan <lfan.canada@gmail.com>
2021-05-19 12:02:32 -0700

    update import path to correct doc.

--- src/components/__tests__/Appointment.test.js
+++ src/components/__tests__/Appointment.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { render } from "@testing-library/react";
-import Appointment from "components/Application";
+import Appointment from "../Appointment/index";
 
 describe("Appointment", () => {
   it("renders without crashing", () => {
