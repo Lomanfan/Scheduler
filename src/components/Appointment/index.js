8aeed7d77e1c328c38c9439a75d7d1808fae143f
Lomanfan <lfan.canada@gmail.com>
2021-05-19 12:46:25 -0700

    add data-testid for testing

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -44,7 +44,7 @@ export default function Appointment(props) {
   }
   
   return(
-    <article className="appointment">
+    <article className="appointment" data-testid="appointment">
     <Header time={props.time}/>
     {/* {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer}/> : <Empty />} */}
 
@@ -67,12 +67,12 @@ export default function Appointment(props) {
       )}
     {mode === SAVING && (
       <Status 
-      message="Saving..."
+      message="Saving"
       />
       )}
     {mode === DELETING && (
       <Status 
-      message="Deleting..."
+      message="Deleting"
       />
       )}
       {mode === CONFIRM && (
